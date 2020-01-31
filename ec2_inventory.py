2d52140c8bc545b7dd7cb21b6f7249f29527f0a1
root <root@ip-172-31-19-178.eu-west-1.compute.internal>
2020-01-31 11:06:24 +0000

    fix inventory

--- ec2_inventory.py
+++ ec2_inventory.py
@@ -19,15 +19,18 @@ def main():
         client = boto3.client('ec2', region_name=region)
 	f = open("hosts","w+")
         
-	response = client.describe_instances(DryRun=False, Filters = [{ 'Name':'instance-state-code', 'Values': ['16']}])
-        for i in response['Reservations']:
-    	    name = getTag(i['Instances'][0]['Tags'], "Name")
-    	    ip = i['Instances'][0]['PublicIpAddress']
-    	    instanceid=i['Instances'][0]['InstanceId']
+	response = client.describe_instances(DryRun=False) #, Filters = [{ 'Name':'instance-state-code', 'Values': ['16']}])
+        for i2 in response['Reservations']:
+    	    for i in i2['Instances']:
+    		#print(i)
+    		name = getTag(i['Tags'], "Name")
+    		print(name + "\n")
+    		ip = i['PublicIpAddress']
+    		instanceid=i['InstanceId']
         
-            #hosts['list'].append( {'name':name, 'ip':ip, 'instanceId':instanceid})
-	    f.write("#" + name + "\n")
-    	    f.write(ip + " ansible_ssh_user=ec2-user istanceId=" + instanceid + " region=" + region + "\n\n")
+        	#hosts['list'].append( {'name':name, 'ip':ip, 'instanceId':instanceid})
+		f.write("#" + name + "\n")
+    		f.write(ip + " ansible_ssh_user=ec2-user istanceId=" + instanceid + " region=" + region + "\n\n")
 
 
 
